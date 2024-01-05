e5618294b022e4594b0cae2d0f508e85d0f29544
aguswesly <agustchannel@gmail.com>
2024-01-05 09:05:56 +0700

    fix : adjust input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-smtransition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-9 w-full rounded-md border border-neutral-400 bg-transparent px-3 py-1 text-smtransition-colors placeholder:placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-900 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
